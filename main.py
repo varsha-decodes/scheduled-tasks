41f04f14e6c3f5f7396b653f7e587266a42024c2
Varsha Hari <varshahari5002@gmail.com>
2026-05-27 16:31:52 +0530

    Integrate weather API and SMS notification
    Added functionality to fetch weather details and send SMS alerts if rain is forecasted.

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+api_key = os.environ.get("API_KEY")
+api_params = {
+    'lat':21.932767,
+    'lon':86.681493,
+    'cnt':4,
+    'appid': api_key
+}
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+def get_details():
+    api_call = "https://api.openweathermap.org/data/2.5/forecast"
+    response = requests.get(api_call, params=api_params)
+    response.raise_for_status()
+    weather_forecast = response.json()
+    will_rain = False
+    for day in weather_forecast['list']:
+        if day['weather'][0]['id']<700:
+            will_rain = True
+    if will_rain:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            from_='+19204826193',
+            body="Don't forget to take an umbrella :)",
+            to='+919940901332'
         )
+        print(message.status)
+get_details()
